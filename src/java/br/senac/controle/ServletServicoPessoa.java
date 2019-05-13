d9e00445bb7708b0c58a1dff72f1209461123278
80119050 <80119050@MSC36171427.correiosnet.int>
2019-05-13 10:40:07 -0300

    Teste de commit no NetBeans!

--- src/java/br/senac/controle/ServletServicoPessoa.java
+++ src/java/br/senac/controle/ServletServicoPessoa.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package br.senac.controle;
-
 import br.senac.entidade.MainLeitura;
 import br.senac.entidade.Pessoa;
 import com.google.gson.Gson;
@@ -23,15 +22,6 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class ServletServicoPessoa extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         //response.setContentType("text/html;charset=UTF-8");
