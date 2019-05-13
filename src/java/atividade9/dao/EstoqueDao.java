1e3f7193442bff751be7e0fae01df35c73226703
80119050 <80119050@MSC36171427.correiosnet.int>
2019-05-13 11:16:49 -0300

    Teste de commit no NetBeans!

--- src/java/atividade9/dao/EstoqueDao.java
+++ src/java/atividade9/dao/EstoqueDao.java
@@ -5,6 +5,7 @@
  */
 package atividade9.dao;
 
+import atividade9.entidade.Estoque;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -16,6 +17,40 @@ import java.util.ArrayList;
  * @author 80119050
  */
 public class EstoqueDao {
+
+    public ArrayList<Estoque> listarEstoque() {
+        
+        String query = "select * from itens";
+
+        Connection conn = ConexaoComBanco.getConnection();
+        PreparedStatement prepStmt = ConexaoComBanco.getPreparedStatement(conn, query);
+
+        ArrayList<Estoque> listaDeItens = new ArrayList<Estoque>();
+
+        try {
+            ResultSet resultado = prepStmt.executeQuery(query);
+
+            while (resultado.next()) {
+
+                Estoque estoque = new Estoque();
+                estoque.setCodigoDoEstoque(resultado.getInt(1));
+                estoque.setNome(resultado.getString("nome"));
+                estoque.setDescricao(resultado.getString("descricao"));
+                estoque.setValor(resultado.getDouble("valor"));
+                estoque.setDescricao(resultado.getString("email"));
+                
+
+                listaDeItens.add(estoque);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return listaDeItens;
+        
+        
+    }
     
     
     
